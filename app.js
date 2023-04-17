78b0f25d769a1cdca3f6534097b1d1ca7ce575d3
ali jafari <jafari.8001.ali@gmail.com>
2023-04-18 01:01:31 +0330

    complet the form design & write get voices function in app.js

--- app.js
+++ app.js
@@ -1,2 +1,32 @@
-const speech = window.speechSynthesis;
+//init speech
+const synth = window.speechSynthesis;
 
+//DOM elements 
+
+const textForm = document.querySelector('form');
+const textInput = document.querySelector('#text-input');
+const voiceSelect = document.querySelector('#voice-select');
+const rate = document.querySelector('#rate');
+const rateValue = document.querySelector('#rate-value');
+const pitch = document.querySelector('#pitch');
+const pitchValue = document.querySelector('#pitch-value');
+
+// init voices array
+let voices = [];
+
+const getVoices =()=>{
+    voices = synth.getVoices();
+    // loop 
+    voices.forEach(voice =>{
+        const option = document.createElement('option');
+        option.textContent = voice.name + '('+ voice.lang + ')';
+        option.setAttribute('data-lang', voice.lang);
+        option.setAttribute('data-name', voice.name);
+        voiceSelect.appendChild(option);
+        })
+}
+
+getVoices();
+if (synth.onvoiceschanged !== undefined) {
+    synth.onvoiceschanged = getVoices;
+}
