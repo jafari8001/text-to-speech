1166ce48c2d5fa19f4d42ad1de17621d9a708950
ali jafari <jafari.8001.ali@gmail.com>
2023-04-19 17:59:51 +0330

    complet App.js and compelet the design

--- app.js
+++ app.js
@@ -30,3 +30,55 @@ getVoices();
 if (synth.onvoiceschanged !== undefined) {
     synth.onvoiceschanged = getVoices;
 }
+
+// speak
+const speak = ()=>{
+    if (synth.speaking) {
+        console.error('Already...');
+        return;
+    }
+    if (textInput.value !== '') {
+        // set speak text 
+        const speakText = new SpeechSynthesisUtterance(textInput.value);
+        // speak end
+        speakText.onend = e=>{
+            console.log('Done speaking...');
+        }
+
+        // speak error
+        speakText.onerror= e=>{
+            console.log('Error...');
+        }
+
+        // select voice
+        const selectedVoice = voiceSelect.selectedOptions[0].getAttribute('data-name');
+
+        // loop 
+        voices.forEach(voice =>{
+            if (voice.name === selectedVoice) {
+                speakText.voice = voice;
+            }
+        })
+
+        //set pitch & rate
+        speakText.rate = rate.value;
+        speakText.pitch = pitch.value;
+
+        // speak 
+        synth.speak(speakText);
+    }
+}
+
+// Event
+textForm.addEventListener('submit', e=>{
+    e.preventDefault();
+    speak(0);
+    textInput.getBoundingClientRect();
+})
+
+// pitch and rate change
+rate.addEventListener('change', e => rateValue.textContent = rate.value);
+pitch.addEventListener('change', e => pitchValue.textContent = pitch.value);
+
+// voice select change
+voiceSelect.addEventListener('change', e=> speak());
